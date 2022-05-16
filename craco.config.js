838a8b39e31522693871340fb866f78dbeb90559
avdgupta <103413056+avdgupta@users.noreply.github.com>
2022-05-16 12:09:00 +0530

    fixing constants issue

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,7 @@ module.exports = {
     webpack: {
         alias: {
             "@components": path.resolve(__dirname, "src/components"),
-            "@constants": path.resolve(__dirname, "src/constants"),
+            "@constants": path.resolve(__dirname, "src/Constants"),
             "@containers": path.resolve(__dirname, "src/containers"),
             "@hooks": path.resolve(__dirname, "src/hooks"),
             "@store": path.resolve(__dirname, "src/store"),
