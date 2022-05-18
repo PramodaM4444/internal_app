0c6209bd9ff5821a175fb21b6c18c0569266e406
avdgupta <103413056+avdgupta@users.noreply.github.com>
2022-05-18 13:41:59 +0530

    changes

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,7 @@ module.exports = {
     webpack: {
         alias: {
             "@components": path.resolve(__dirname, "src/components"),
-            "@constants": path.resolve(__dirname, "src/Constants"),
+            "@constants": path.resolve(__dirname, "src/constants"),
             "@containers": path.resolve(__dirname, "src/containers"),
             "@hooks": path.resolve(__dirname, "src/hooks"),
             "@store": path.resolve(__dirname, "src/store"),
