255a54f3c1560ca9649ac69530c41dda7d1d12d3
PramodaM4444 <Pramoda.M@ibm.com>
2022-05-20 13:11:22 +0530

    Added types for loading and cart items

---
+++ src/store/reducers/__types__/cartTypes.ts
@@ -0,0 +1,5 @@
+import { EachCartItem } from "@components/CartItem/CartItem.types";
+
+export interface CartItems {
+    cartItems: EachCartItem[]
+}
--- src/store/reducers/loadingReducer.ts
+++ src/store/reducers/loadingReducer.ts
@@ -1,10 +1,11 @@
+import { AnyAction } from "@reduxjs/toolkit";
 import { Loading } from "./__types__/loadingTypes";
 
 const initialState: Loading = {
     isLoading: false,
 };
 
-const loadingReducer = (state: Loading = initialState, action: any) => {
+const loadingReducer = (state: Loading = initialState, action: AnyAction) => {
     const actionType = action.type.split("_").reverse()[0];
 
     switch (actionType) {
