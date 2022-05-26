13cec05df1269142b34d74f36b6d1e026b0d0cf1
PramodaM4444 <Pramoda.M@ibm.com>
2022-05-26 14:22:39 +0530

    Added okta authentication

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Example from "@containers/Example/Example";
+import Logout from "@components/Logout/Logout";
+import { useUserInfo } from "@hooks/userInfo";
 
-export const Home: React.FC = () => (
-    <>
-        <div>From Home </div>
-        <Link to="/login">To Login Page</Link>
-        <Link to="/Forum">Forum Page</Link>
-        <Example />
-    </>
-);
+export const Home: React.FC = () => {
+    const [userInfo] = useUserInfo();
+    return (
+        <>
+            {userInfo && <h1>Hello {userInfo.given_name}</h1>}
+            <div>From Home </div>
+            <Link to="/login">To Login Page</Link>
+            <Link to="/Forum">Forum Page</Link>
+            <Example />
+            <Logout />
+        </>
+    );
+};
