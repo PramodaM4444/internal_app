8be97b00310cd291169440e4943ed9a46c3be402
avdgupta <103413056+avdgupta@users.noreply.github.com>
2022-10-13 20:29:36 +0530

    Fix for blank page issue with empty API response in Home page (#37)

--- src/containers/Home/CardBoxList.tsx
+++ src/containers/Home/CardBoxList.tsx
@@ -1,40 +1,48 @@
+import { LoadingIndicator } from "@components/LoadingIndicator/LoadingIndicator";
 import { useAppDispatch, useAppSelector } from "@hooks/hooks";
 import { fetchGetRecognitionRequest } from "@store/actions/recoginitionAction";
+import { selectIsLoading } from "@store/selectors/loadingSelector";
 import { selectRecognitionData } from "@store/selectors/recognitionSelector";
 import { useEffect } from "react";
 import { CardBox } from "./CardBox";
 
 export const CardBoxList: React.FC = () => {
     const dispatch = useAppDispatch();
+    const isLoading = useAppSelector(selectIsLoading);
 
     useEffect(() => {
         dispatch(fetchGetRecognitionRequest());
-    });
+    }, []);
 
     const recognitionInfo = useAppSelector(selectRecognitionData);
     return (
-        <div
-            style={{
-                display: "flex",
-                flexWrap: "wrap",
-            }}
-        >
-            {recognitionInfo.map(
-                ({
-                    employee_award_name,
-                    employee_award_title,
-                    employee_IBM_Notes_ID,
-                    employee_account,
-                }) => (
-                    <CardBox
-                        employeeImage="/test.jpg"
-                        employeeAwardName={employee_award_name}
-                        employeeAwardTitle={employee_award_title}
-                        employeeIBMNotesID={employee_IBM_Notes_ID}
-                        employeeAccount={employee_account}
-                    />
-                ),
-            )}
-        </div>
+        <>
+            {isLoading && <LoadingIndicator />}
+            <div
+                style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                }}
+            >
+                {Array.isArray(recognitionInfo)
+                    ? recognitionInfo.map(
+                          ({
+                              employee_award_name,
+                              employee_award_title,
+                              employee_IBM_Notes_ID,
+                              employee_account,
+                          }) => (
+                              <CardBox
+                                  employeeImage="/test.jpg"
+                                  employeeAwardName={employee_award_name}
+                                  employeeAwardTitle={employee_award_title}
+                                  employeeIBMNotesID={employee_IBM_Notes_ID}
+                                  employeeAccount={employee_account}
+                              />
+                          ),
+                      )
+                    : null}
+            </div>
+        </>
     );
 };
