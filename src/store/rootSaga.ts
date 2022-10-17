f3ec12ffbae9202020d19614654e53e3406387ed
PramodaM4444 <Pramoda.M@ibm.com>
2022-10-17 12:31:22 +0530

    Fix for login api integration

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,5 +1,4 @@
 import { all, fork } from "redux-saga/effects";
-import userSaga from "./sagas/userSagas";
 import timesheetSaga from "./sagas/timesheetSagas";
 import viewTimesheetSaga from "./sagas/viewTimesheetSagas";
 import userLoginSaga from "./sagas/loginSaga";
@@ -7,10 +6,9 @@ import getEmployeesSaga from "./sagas/getEmployeesSagas";
 import recognitionSaga from "./sagas/recognitionSagas";
 
 export function* rootSaga() {
-    // yield all([fork(userSaga)]);
     yield all([fork(timesheetSaga)]);
     yield all([fork(viewTimesheetSaga)]);
     yield all([fork(getEmployeesSaga)]);
-    yield all([fork(userSaga), fork(userLoginSaga)]);
+    yield all([fork(userLoginSaga)]);
     yield all([fork(recognitionSaga)]);
 }
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -2,6 +2,18 @@ import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 export default async function AxiosAPI(config: AxiosRequestConfig) {
     return axios(config)
-        .then((res: AxiosResponse) => res.data)
+        .then((res: AxiosResponse) => {
+            console.log("ApI response-----", res);
+            return res.data;
+        })
         .catch((err: AxiosError) => err);
+}
+
+export async function LoginAxiosAPI(config: AxiosRequestConfig) {
+    return axios(config)
+        .then((res: AxiosResponse) => ({
+            response: res.data,
+            error: undefined,
+        }))
+        .catch((err: AxiosError) => ({ response: undefined, error: err }));
 }
