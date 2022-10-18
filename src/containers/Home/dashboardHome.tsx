8d14dff7c8609c7d7179160e5f0e5f00e7be2601
Avdhesh Gupta1 <avdhesh.gupta1@ibm.com>
2022-10-18 14:28:28 +0530

    Adding carousel

--- src/containers/Home/dashboardHome.tsx
+++ src/containers/Home/dashboardHome.tsx
@@ -7,21 +7,123 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { UIConstants } from "@constants/UIConstants";
 import { CardBoxList } from "./CardBoxList";
+import { useTheme } from "@mui/material/styles";
+import { Box, Button, MobileStepper } from "@mui/material";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+
 /**
  * Shows Project Information screen
  * @param no_prop not applicable
  * @returns displays the Project Information (along with Accordion) screen: parent: dashboard, child: ProjectDetails
  */
 export const DashboardHome = () => {
+    const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+    const images = [
+        {
+            imgPath:
+                "https://c4.wallpaperflare.com/wallpaper/461/141/233/computer-ibm-wallpaper-preview.jpg",
+        },
+        {
+            imgPath:
+                "https://c0.wallpaperflare.com/preview/811/935/645/adults-analysis-brainstorming-collaboration.jpg",
+        },
+        {
+            imgPath:
+                "https://c4.wallpaperflare.com/wallpaper/247/916/934/computer-ibm-wallpaper-preview.jpg",
+        },
+        {
+            imgPath:
+                "https://c4.wallpaperflare.com/wallpaper/662/51/829/ibm-technology-company-computer-wallpaper-preview.jpg",
+        },
+    ];
+
     const [expanded, setExpanded] = useState("panel2");
 
     const handleChange = (panel: any) => (event: any, isExpanded: any) => {
         setExpanded(isExpanded ? panel : "");
     };
 
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = images.length;
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step: number) => {
+        setActiveStep(step);
+    };
+
     return (
         <Grid container spacing={3}>
             <Grid item xs={12} md={8} lg={12}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <AutoPlaySwipeableViews
+                        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+                        index={activeStep}
+                        onChangeIndex={handleStepChange}
+                        enableMouseEvents
+                    >
+                        {images.map((step, index) => (
+                            <div>
+                                {Math.abs(activeStep - index) <= 2 ? (
+                                    <Grid
+                                        component="img"
+                                        sx={{
+                                            height: 450,
+                                            display: "block",
+                                            overflow: "hidden",
+                                            width: "100%",
+                                        }}
+                                        src={step.imgPath}
+                                    />
+                                ) : null}
+                                <MobileStepper
+                                    steps={maxSteps}
+                                    position="static"
+                                    activeStep={activeStep}
+                                    nextButton={
+                                        <Button
+                                            size="small"
+                                            onClick={handleNext}
+                                            disabled={
+                                                activeStep === maxSteps - 1
+                                            }
+                                        >
+                                            {theme.direction === "rtl" ? (
+                                                <KeyboardArrowLeft />
+                                            ) : (
+                                                <KeyboardArrowRight />
+                                            )}
+                                        </Button>
+                                    }
+                                    backButton={
+                                        <Button
+                                            size="small"
+                                            onClick={handleBack}
+                                            disabled={activeStep === 0}
+                                        >
+                                            {theme.direction === "rtl" ? (
+                                                <KeyboardArrowRight />
+                                            ) : (
+                                                <KeyboardArrowLeft />
+                                            )}
+                                        </Button>
+                                    }
+                                />
+                            </div>
+                        ))}
+                    </AutoPlaySwipeableViews>
+                </Box>
+
                 <Accordion
                     expanded={expanded === "panel2"}
                     onChange={handleChange("panel2")}
